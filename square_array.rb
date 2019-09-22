dd6c5edc19de3e432459e2263574a0b64648a1b1
Alyssa Yates <bosslys@icloud.com>
2019-09-22 19:17:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,2 +1,24 @@
-def square_array
+
+def square_array(array)
+  new_numbers = []
 end 
+
+
+
+
+
+
+
+
+
+
+
+# 
+# 
+# def square_array(array)
+#   new_numbers = [ ]
+#   array.each do |num|
+#     new_numbers << (num ** 2)
+#   end
+#   return new_numbers
+# end
